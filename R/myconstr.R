49c3b001f9670da9aa6e7790fe884d5cafed51e7
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-04-26 20:48:51 -0400

    updated

--- R/myconstr.R
+++ R/myconstr.R
@@ -1,3 +1,12 @@
+#' This is my constructor function
+#'
+#' @param x
+#' @param y
+#'
+#' @return
+#' @export
+#'
+#' @examples
 myconstr = function(x,y){
   ylm = lm(y~x)
   obj = list(data = list(x = x, y = y), ylm = ylm)
